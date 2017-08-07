ef23bd66a43ff1fbfc71f63db6dc9cc3ab30a0b0
German Martin <martin@ais.uni-bonn.de>
2017-08-07 14:52:04 +0200

    second commit

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/martin/workspace_tests/src/capture_data
-# Build directory: /home/martin/workspace_tests/src/capture_data/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "roslib".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "capture_data"
-PROJECT_SPACE_DIR = "/home/martin/workspace_tests/src/capture_data/build/devel"
-PROJECT_VERSION = "0.0.0"
